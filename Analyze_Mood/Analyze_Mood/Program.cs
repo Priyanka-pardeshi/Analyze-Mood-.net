c4cb697f90687d3697bd0de4383459df759df150
Priyanka-pardeshi <priyankasspardeshi@gmail.com>
2021-06-24 18:13:50 +0530

    Welcome to mood analysis

---
+++ Analyze_Mood/Analyze_Mood/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Analyze_Mood
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Mood analyzer System");
+        }
+    }
+}
